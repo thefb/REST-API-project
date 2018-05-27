1b5a24c33dbfb839807eb4d229f53649eb620bae
Fabiano Siqueira <fabiano.siqueirab@gmail.com>
2018-05-27 16:27:24 -0300

    test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from resources.loja import Lojas, LojaLista
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') #, 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgres://qzuzwybxiwakwf:0c878e82f7b3a9059a10f46132181bf9211087a91cb57069586526931d5f7667@ec2-54-204-39-46.compute-1.amazonaws.com:5432/dr0rj9rmpu6vr', 'sqlite:///data.db') #, '
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'fabs'
 api = Api(app)
